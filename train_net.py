f80881f86ee6dc2df688e86da7d9b980605d28c2
rjn991 <ranjanht991@gmail.com>
2024-03-13 16:11:23 +0530

    save model to drive

--- train_net.py
+++ train_net.py
@@ -130,6 +130,9 @@ def train_net(cfg):
                     if isinstance(m, Basenet):
                         filepath=cfg.result_path+'/stage%d_epoch%d_%.2f%%.pth'%(cfg.training_stage,epoch,test_info['activities_acc'])
                         m.savemodel(filepath)
+                        if epoch%10==0:
+                            filepath='/content/drive/MyDrive/models'+'/stage%d_epoch%d_%.2f%%.pth'%(cfg.training_stage,epoch,test_info['activities_acc'])
+                            m.savemodel(filepath)
 #                         print('model saved to:',filepath)
             else:
                 assert False
